19a16aec5f090d6463762949d07f21c812a0ef8c
Maitreya Ranganath <maitreyr@amazon.com>
2014-03-05 21:57:18 -0600

    Changed port to 80.

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-var server = app.listen(3000, function() {
+var server = app.listen(80, function() {
         console.log('Listening on port %d', server.address().port);
 });
